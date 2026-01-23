e7eb533f257d69988a67d95c996517f8c2bca2f5
Souradeep Nanda <souradeep.nanda123@gmail.com>
2026-01-23 20:09:30 +0530

    assert the meta

--- src/attach-fragment.test.js
+++ src/attach-fragment.test.js
@@ -123,20 +123,49 @@ test('attachFragmentAt - attach CH3 to benzene at position 1', async () => {
   const benzene = await Fragment('c1ccccc1');
   const methyl = await Fragment('C');
 
-  const benzeneMeta = benzene.meta;
-
   const toluene = await attachFragmentAt(benzene, 1, methyl, { FragmentConstructor: Fragment });
 
-  // Assert benzene has 6 atoms before
-  assert.strictEqual(benzeneMeta.molecules[0].atoms.length, 6);
-  assert.strictEqual(benzeneMeta.molecules[0].bonds.length, 6);
-
-  // Assert toluene structure after
   assert.strictEqual(toluene.atoms, 7);
   assert.strictEqual(toluene.rings, 1);
   assert.strictEqual(toluene.formula, 'C7H8');
-  assert.strictEqual(toluene.meta.molecules[0].atoms.length, 7);
-  assert.strictEqual(toluene.meta.molecules[0].bonds.length, 7);
+  assert.deepStrictEqual(toluene.meta, {
+    rdkitjson: { version: 12 },
+    defaults: {
+      atom: {
+        z: 6, impHs: 0, chg: 0, nRad: 0, isotope: 0, stereo: 'unspecified',
+      },
+      bond: { bo: 1, stereo: 'unspecified' },
+    },
+    molecules: [{
+      atoms: [
+        { impHs: 3 },
+        {},
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+      ],
+      bonds: [
+        { atoms: [0, 1] },
+        { bo: 2, atoms: [1, 2] },
+        { atoms: [2, 3] },
+        { bo: 2, atoms: [3, 4] },
+        { atoms: [4, 5] },
+        { bo: 2, atoms: [5, 6] },
+        { atoms: [6, 1] },
+      ],
+      extensions: [{
+        name: 'rdkitRepresentation',
+        formatVersion: 2,
+        toolkitVersion: '2025.03.4',
+        aromaticAtoms: [1, 2, 3, 4, 5, 6],
+        aromaticBonds: [1, 2, 3, 4, 5, 6],
+        cipRanks: [0, 4, 3, 2, 1, 2, 3],
+        atomRings: [[1, 6, 5, 4, 3, 2]],
+      }],
+    }],
+  });
 });
 
 test('attachFragmentAt - attach two CH3 groups to benzene at positions 0 and 3 to make xylene', async () => {
@@ -144,129 +173,261 @@ test('attachFragmentAt - attach two CH3 groups to benzene at positions 0 and 3 t
   const methyl1 = await Fragment('C');
   const methyl2 = await Fragment('C');
 
-  const benzeneMeta = benzene.meta;
-
   const withFirstMethyl = await attachFragmentAt(benzene, 0, methyl1, {
     FragmentConstructor: Fragment,
   });
-  const withFirstMethylMeta = withFirstMethyl.meta;
 
   const xylene = await attachFragmentAt(withFirstMethyl, 3, methyl2, {
     FragmentConstructor: Fragment,
   });
 
-  // Assert benzene before
-  assert.strictEqual(benzeneMeta.molecules[0].atoms.length, 6);
-
-  // Assert intermediate (toluene) has 7 atoms
-  assert.strictEqual(withFirstMethylMeta.molecules[0].atoms.length, 7);
-  assert.strictEqual(withFirstMethylMeta.molecules[0].bonds.length, 7);
-
-  // Assert xylene after
   assert.strictEqual(xylene.atoms, 8);
   assert.strictEqual(xylene.rings, 1);
   assert.strictEqual(xylene.formula, 'C8H10');
-  assert.strictEqual(xylene.meta.molecules[0].atoms.length, 8);
-  assert.strictEqual(xylene.meta.molecules[0].bonds.length, 8);
+  assert.deepStrictEqual(xylene.meta, {
+    rdkitjson: { version: 12 },
+    defaults: {
+      atom: {
+        z: 6, impHs: 0, chg: 0, nRad: 0, isotope: 0, stereo: 'unspecified',
+      },
+      bond: { bo: 1, stereo: 'unspecified' },
+    },
+    molecules: [{
+      atoms: [
+        { impHs: 3 },
+        {},
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+        {},
+        { impHs: 3 },
+        { impHs: 1 },
+      ],
+      bonds: [
+        { atoms: [0, 1] },
+        { bo: 2, atoms: [1, 2] },
+        { atoms: [2, 3] },
+        { bo: 2, atoms: [3, 4] },
+        { atoms: [4, 5] },
+        { atoms: [5, 6] },
+        { bo: 2, atoms: [5, 7] },
+        { atoms: [7, 1] },
+      ],
+      extensions: [{
+        name: 'rdkitRepresentation',
+        formatVersion: 2,
+        toolkitVersion: '2025.03.4',
+        aromaticAtoms: [1, 2, 3, 4, 5, 7],
+        aromaticBonds: [1, 2, 3, 4, 6, 7],
+        cipRanks: [0, 4, 2, 1, 2, 4, 0, 3],
+        atomRings: [[1, 7, 5, 4, 3, 2]],
+      }],
+    }],
+  });
 });
 
 test('attachFragmentAt - attach OH to benzene at position 0 to make phenol', async () => {
   const benzene = await Fragment('c1ccccc1');
   const hydroxyl = await Fragment('O');
 
-  const benzeneMeta = benzene.meta;
-  const hydroxylMeta = hydroxyl.meta;
-
   const phenol = await attachFragmentAt(benzene, 0, hydroxyl, { FragmentConstructor: Fragment });
 
-  // Assert benzene before
-  assert.strictEqual(benzeneMeta.molecules[0].atoms.length, 6);
-  assert.strictEqual(benzeneMeta.molecules[0].atoms[0].impHs, 1);
-
-  // Assert hydroxyl (oxygen with 1 implicit H)
-  assert.strictEqual(hydroxylMeta.molecules[0].atoms.length, 1);
-  assert.strictEqual(hydroxylMeta.molecules[0].atoms[0].z, 8); // Oxygen
-
-  // Assert phenol after
   assert.strictEqual(phenol.atoms, 7);
   assert.strictEqual(phenol.rings, 1);
   assert.strictEqual(phenol.formula, 'C6H6O');
-  assert.strictEqual(phenol.meta.molecules[0].atoms.length, 7);
-  assert.strictEqual(phenol.meta.molecules[0].bonds.length, 7);
+  assert.deepStrictEqual(phenol.meta, {
+    rdkitjson: { version: 12 },
+    defaults: {
+      atom: {
+        z: 6, impHs: 0, chg: 0, nRad: 0, isotope: 0, stereo: 'unspecified',
+      },
+      bond: { bo: 1, stereo: 'unspecified' },
+    },
+    molecules: [{
+      atoms: [
+        { z: 8, impHs: 1 },
+        {},
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+      ],
+      bonds: [
+        { atoms: [0, 1] },
+        { bo: 2, atoms: [1, 2] },
+        { atoms: [2, 3] },
+        { bo: 2, atoms: [3, 4] },
+        { atoms: [4, 5] },
+        { bo: 2, atoms: [5, 6] },
+        { atoms: [6, 1] },
+      ],
+      extensions: [{
+        name: 'rdkitRepresentation',
+        formatVersion: 2,
+        toolkitVersion: '2025.03.4',
+        aromaticAtoms: [1, 2, 3, 4, 5, 6],
+        aromaticBonds: [1, 2, 3, 4, 5, 6],
+        cipRanks: [4, 3, 2, 1, 0, 1, 2],
+        atomRings: [[1, 6, 5, 4, 3, 2]],
+      }],
+    }],
+  });
 });
 
 test('attachFragmentAt - attach NH2 to benzene at position 0 to make aniline', async () => {
   const benzene = await Fragment('c1ccccc1');
   const amino = await Fragment('N');
 
-  const benzeneMeta = benzene.meta;
-  const aminoMeta = amino.meta;
-
   const aniline = await attachFragmentAt(benzene, 0, amino, { FragmentConstructor: Fragment });
 
-  // Assert benzene before
-  assert.strictEqual(benzeneMeta.molecules[0].atoms.length, 6);
-
-  // Assert amino (nitrogen with 2 implicit H)
-  assert.strictEqual(aminoMeta.molecules[0].atoms.length, 1);
-  assert.strictEqual(aminoMeta.molecules[0].atoms[0].z, 7); // Nitrogen
-
-  // Assert aniline after
   assert.strictEqual(aniline.atoms, 7);
   assert.strictEqual(aniline.rings, 1);
   assert.strictEqual(aniline.formula, 'C6H7N');
-  assert.strictEqual(aniline.meta.molecules[0].atoms.length, 7);
-  assert.strictEqual(aniline.meta.molecules[0].bonds.length, 7);
+  assert.deepStrictEqual(aniline.meta, {
+    rdkitjson: { version: 12 },
+    defaults: {
+      atom: {
+        z: 6, impHs: 0, chg: 0, nRad: 0, isotope: 0, stereo: 'unspecified',
+      },
+      bond: { bo: 1, stereo: 'unspecified' },
+    },
+    molecules: [{
+      atoms: [
+        { z: 7, impHs: 2 },
+        {},
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+      ],
+      bonds: [
+        { atoms: [0, 1] },
+        { bo: 2, atoms: [1, 2] },
+        { atoms: [2, 3] },
+        { bo: 2, atoms: [3, 4] },
+        { atoms: [4, 5] },
+        { bo: 2, atoms: [5, 6] },
+        { atoms: [6, 1] },
+      ],
+      extensions: [{
+        name: 'rdkitRepresentation',
+        formatVersion: 2,
+        toolkitVersion: '2025.03.4',
+        aromaticAtoms: [1, 2, 3, 4, 5, 6],
+        aromaticBonds: [1, 2, 3, 4, 5, 6],
+        cipRanks: [4, 3, 2, 1, 0, 1, 2],
+        atomRings: [[1, 6, 5, 4, 3, 2]],
+      }],
+    }],
+  });
 });
 
 test('attachFragmentAt - attach ethyl (CC) to benzene at position 0 to make ethylbenzene', async () => {
   const benzene = await Fragment('c1ccccc1');
   const ethyl = await Fragment('CC');
 
-  const benzeneMeta = benzene.meta;
-  const ethylMeta = ethyl.meta;
-
   const ethylbenzene = await attachFragmentAt(benzene, 0, ethyl, { FragmentConstructor: Fragment });
 
-  // Assert benzene before
-  assert.strictEqual(benzeneMeta.molecules[0].atoms.length, 6);
-
-  // Assert ethyl (2 carbons)
-  assert.strictEqual(ethylMeta.molecules[0].atoms.length, 2);
-  assert.strictEqual(ethylMeta.molecules[0].bonds.length, 1);
-
-  // Assert ethylbenzene after
   assert.strictEqual(ethylbenzene.atoms, 8);
   assert.strictEqual(ethylbenzene.rings, 1);
   assert.strictEqual(ethylbenzene.formula, 'C8H10');
-  assert.strictEqual(ethylbenzene.meta.molecules[0].atoms.length, 8);
-  assert.strictEqual(ethylbenzene.meta.molecules[0].bonds.length, 8);
+  assert.deepStrictEqual(ethylbenzene.meta, {
+    rdkitjson: { version: 12 },
+    defaults: {
+      atom: {
+        z: 6, impHs: 0, chg: 0, nRad: 0, isotope: 0, stereo: 'unspecified',
+      },
+      bond: { bo: 1, stereo: 'unspecified' },
+    },
+    molecules: [{
+      atoms: [
+        { impHs: 3 },
+        { impHs: 2 },
+        {},
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+      ],
+      bonds: [
+        { atoms: [0, 1] },
+        { atoms: [1, 2] },
+        { bo: 2, atoms: [2, 3] },
+        { atoms: [3, 4] },
+        { bo: 2, atoms: [4, 5] },
+        { atoms: [5, 6] },
+        { bo: 2, atoms: [6, 7] },
+        { atoms: [7, 2] },
+      ],
+      extensions: [{
+        name: 'rdkitRepresentation',
+        formatVersion: 2,
+        toolkitVersion: '2025.03.4',
+        aromaticAtoms: [2, 3, 4, 5, 6, 7],
+        aromaticBonds: [2, 3, 4, 5, 6, 7],
+        cipRanks: [0, 1, 5, 4, 3, 2, 3, 4],
+        atomRings: [[2, 7, 6, 5, 4, 3]],
+      }],
+    }],
+  });
 });
 
 test('attachFragmentAt - attach propyl (CCC) to benzene to make propylbenzene', async () => {
   const benzene = await Fragment('c1ccccc1');
   const propyl = await Fragment('CCC');
 
-  const benzeneMeta = benzene.meta;
-  const propylMeta = propyl.meta;
-
   const propylbenzene = await attachFragmentAt(benzene, 0, propyl, {
     FragmentConstructor: Fragment,
   });
 
-  // Assert benzene before
-  assert.strictEqual(benzeneMeta.molecules[0].atoms.length, 6);
-
-  // Assert propyl (3 carbons)
-  assert.strictEqual(propylMeta.molecules[0].atoms.length, 3);
-  assert.strictEqual(propylMeta.molecules[0].bonds.length, 2);
-
-  // Assert propylbenzene after
   assert.strictEqual(propylbenzene.atoms, 9);
   assert.strictEqual(propylbenzene.rings, 1);
   assert.strictEqual(propylbenzene.formula, 'C9H12');
-  assert.strictEqual(propylbenzene.meta.molecules[0].atoms.length, 9);
-  assert.strictEqual(propylbenzene.meta.molecules[0].bonds.length, 9);
+  assert.deepStrictEqual(propylbenzene.meta, {
+    rdkitjson: { version: 12 },
+    defaults: {
+      atom: {
+        z: 6, impHs: 0, chg: 0, nRad: 0, isotope: 0, stereo: 'unspecified',
+      },
+      bond: { bo: 1, stereo: 'unspecified' },
+    },
+    molecules: [{
+      atoms: [
+        { impHs: 3 },
+        { impHs: 2 },
+        { impHs: 2 },
+        {},
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+      ],
+      bonds: [
+        { atoms: [0, 1] },
+        { atoms: [1, 2] },
+        { atoms: [2, 3] },
+        { bo: 2, atoms: [3, 4] },
+        { atoms: [4, 5] },
+        { bo: 2, atoms: [5, 6] },
+        { atoms: [6, 7] },
+        { bo: 2, atoms: [7, 8] },
+        { atoms: [8, 3] },
+      ],
+      extensions: [{
+        name: 'rdkitRepresentation',
+        formatVersion: 2,
+        toolkitVersion: '2025.03.4',
+        aromaticAtoms: [3, 4, 5, 6, 7, 8],
+        aromaticBonds: [3, 4, 5, 6, 7, 8],
+        cipRanks: [0, 1, 2, 6, 5, 4, 3, 4, 5],
+        atomRings: [[3, 8, 7, 6, 5, 4]],
+      }],
+    }],
+  });
 });
 
 test('attachFragmentAt - invalid position throws error', async () => {
@@ -300,4 +461,44 @@ test('attachFragmentAt - attach at position 0 with attachment position specified
 
   assert.strictEqual(ethylbenzene.atoms, 8);
   assert.strictEqual(ethylbenzene.formula, 'C8H10');
+  assert.deepStrictEqual(ethylbenzene.meta, {
+    rdkitjson: { version: 12 },
+    defaults: {
+      atom: {
+        z: 6, impHs: 0, chg: 0, nRad: 0, isotope: 0, stereo: 'unspecified',
+      },
+      bond: { bo: 1, stereo: 'unspecified' },
+    },
+    molecules: [{
+      atoms: [
+        { impHs: 3 },
+        { impHs: 2 },
+        {},
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+      ],
+      bonds: [
+        { atoms: [0, 1] },
+        { atoms: [1, 2] },
+        { bo: 2, atoms: [2, 3] },
+        { atoms: [3, 4] },
+        { bo: 2, atoms: [4, 5] },
+        { atoms: [5, 6] },
+        { bo: 2, atoms: [6, 7] },
+        { atoms: [7, 2] },
+      ],
+      extensions: [{
+        name: 'rdkitRepresentation',
+        formatVersion: 2,
+        toolkitVersion: '2025.03.4',
+        aromaticAtoms: [2, 3, 4, 5, 6, 7],
+        aromaticBonds: [2, 3, 4, 5, 6, 7],
+        cipRanks: [0, 1, 5, 4, 3, 2, 3, 4],
+        atomRings: [[2, 7, 6, 5, 4, 3]],
+      }],
+    }],
+  });
 });
--- src/fuse-fragments.test.js
+++ src/fuse-fragments.test.js
@@ -7,46 +7,112 @@ test('fuseFragmentsAtEdge - fuse benzene to benzene at positions [0,1] to make n
   const benzene1 = await Fragment('c1ccccc1');
   const benzene2 = await Fragment('c1ccccc1');
 
-  const benzene1Meta = benzene1.meta;
-  const benzene2Meta = benzene2.meta;
-
   const naphthalene = await fuseFragmentsAtEdge(benzene1, [0, 1], benzene2, Fragment);
 
-  // Assert benzene before - 6 atoms each
-  assert.strictEqual(benzene1Meta.molecules[0].atoms.length, 6);
-  assert.strictEqual(benzene1Meta.molecules[0].bonds.length, 6);
-  assert.strictEqual(benzene2Meta.molecules[0].atoms.length, 6);
-  assert.strictEqual(benzene2Meta.molecules[0].bonds.length, 6);
-
-  // Assert naphthalene after - 10 atoms (6+6-2 fused)
   assert.strictEqual(naphthalene.atoms, 10);
   assert.strictEqual(naphthalene.rings, 2);
   assert.strictEqual(naphthalene.formula, 'C10H8');
-  assert.strictEqual(naphthalene.meta.molecules[0].atoms.length, 10);
-  // Bonds: 6 (ring1) + 5 (ring2 minus fused edge) = 11
-  assert.strictEqual(naphthalene.meta.molecules[0].bonds.length, 11);
-  assert.strictEqual(naphthalene.meta.molecules[0].extensions[0].atomRings.length, 2);
+  assert.deepStrictEqual(naphthalene.meta, {
+    rdkitjson: { version: 12 },
+    defaults: {
+      atom: {
+        z: 6, impHs: 0, chg: 0, nRad: 0, isotope: 0, stereo: 'unspecified',
+      },
+      bond: { bo: 1, stereo: 'unspecified' },
+    },
+    molecules: [{
+      atoms: [
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+        {},
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+        {},
+        { impHs: 1 },
+      ],
+      bonds: [
+        { bo: 2, atoms: [0, 1] },
+        { atoms: [1, 2] },
+        { bo: 2, atoms: [2, 3] },
+        { atoms: [3, 4] },
+        { bo: 2, atoms: [4, 5] },
+        { atoms: [5, 6] },
+        { bo: 2, atoms: [6, 7] },
+        { atoms: [7, 8] },
+        { bo: 2, atoms: [8, 9] },
+        { atoms: [9, 0] },
+        { atoms: [8, 3] },
+      ],
+      extensions: [{
+        name: 'rdkitRepresentation',
+        formatVersion: 2,
+        toolkitVersion: '2025.03.4',
+        aromaticAtoms: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        aromaticBonds: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        cipRanks: [0, 0, 1, 2, 1, 0, 0, 1, 2, 1],
+        atomRings: [[0, 9, 8, 3, 2, 1], [4, 5, 6, 7, 8, 3]],
+      }],
+    }],
+  });
 });
 
 test('fuseFragmentsAtEdge - fuse benzene to benzene at edge [1,2] to make naphthalene variant', async () => {
   const benzene1 = await Fragment('c1ccccc1');
   const benzene2 = await Fragment('c1ccccc1');
 
-  const benzene1Meta = benzene1.meta;
-
   const naphthalene = await fuseFragmentsAtEdge(benzene1, [1, 2], benzene2, Fragment);
 
-  // Assert benzene before
-  assert.strictEqual(benzene1Meta.molecules[0].atoms.length, 6);
-  assert.strictEqual(benzene1Meta.molecules[0].bonds.length, 6);
-
-  // Assert naphthalene after
   assert.strictEqual(naphthalene.atoms, 10);
   assert.strictEqual(naphthalene.rings, 2);
   assert.strictEqual(naphthalene.formula, 'C10H8');
-  assert.strictEqual(naphthalene.meta.molecules[0].atoms.length, 10);
-  assert.strictEqual(naphthalene.meta.molecules[0].bonds.length, 11);
-  assert.strictEqual(naphthalene.meta.molecules[0].extensions[0].atomRings.length, 2);
+  assert.deepStrictEqual(naphthalene.meta, {
+    rdkitjson: { version: 12 },
+    defaults: {
+      atom: {
+        z: 6, impHs: 0, chg: 0, nRad: 0, isotope: 0, stereo: 'unspecified',
+      },
+      bond: { bo: 1, stereo: 'unspecified' },
+    },
+    molecules: [{
+      atoms: [
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+        {},
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+        {},
+        { impHs: 1 },
+      ],
+      bonds: [
+        { bo: 2, atoms: [0, 1] },
+        { atoms: [1, 2] },
+        { bo: 2, atoms: [2, 3] },
+        { atoms: [3, 4] },
+        { bo: 2, atoms: [4, 5] },
+        { atoms: [5, 6] },
+        { bo: 2, atoms: [6, 7] },
+        { atoms: [7, 8] },
+        { bo: 2, atoms: [8, 9] },
+        { atoms: [9, 0] },
+        { atoms: [8, 3] },
+      ],
+      extensions: [{
+        name: 'rdkitRepresentation',
+        formatVersion: 2,
+        toolkitVersion: '2025.03.4',
+        aromaticAtoms: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        aromaticBonds: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        cipRanks: [0, 0, 1, 2, 1, 0, 0, 1, 2, 1],
+        atomRings: [[0, 9, 8, 3, 2, 1], [4, 5, 6, 7, 8, 3]],
+      }],
+    }],
+  });
 });
 
 test('fuseFragmentsAtEdge - fuse three benzenes to make anthracene', async () => {
@@ -54,27 +120,64 @@ test('fuseFragmentsAtEdge - fuse three benzenes to make anthracene', async () =>
   const benzene2 = await Fragment('c1ccccc1');
   const benzene3 = await Fragment('c1ccccc1');
 
-  const benzene1Meta = benzene1.meta;
-
   const naphthalene = await fuseFragmentsAtEdge(benzene1, [0, 1], benzene2, Fragment);
-  const naphthaleneMeta = naphthalene.meta;
-
   const anthracene = await fuseFragmentsAtEdge(naphthalene, [6, 7], benzene3, Fragment);
 
-  // Assert benzene before
-  assert.strictEqual(benzene1Meta.molecules[0].atoms.length, 6);
-
-  // Assert naphthalene intermediate
-  assert.strictEqual(naphthaleneMeta.molecules[0].atoms.length, 10);
-  assert.strictEqual(naphthaleneMeta.molecules[0].bonds.length, 11);
-  assert.strictEqual(naphthaleneMeta.molecules[0].extensions[0].atomRings.length, 2);
-
-  // Assert anthracene after
   assert.strictEqual(anthracene.atoms, 14);
   assert.strictEqual(anthracene.rings, 3);
   assert.strictEqual(anthracene.formula, 'C14H10');
-  assert.strictEqual(anthracene.meta.molecules[0].atoms.length, 14);
-  // Bonds: 11 (naphthalene) + 5 (benzene minus fused edge) = 16
-  assert.strictEqual(anthracene.meta.molecules[0].bonds.length, 16);
-  assert.strictEqual(anthracene.meta.molecules[0].extensions[0].atomRings.length, 3);
+  assert.deepStrictEqual(anthracene.meta, {
+    rdkitjson: { version: 12 },
+    defaults: {
+      atom: {
+        z: 6, impHs: 0, chg: 0, nRad: 0, isotope: 0, stereo: 'unspecified',
+      },
+      bond: { bo: 1, stereo: 'unspecified' },
+    },
+    molecules: [{
+      atoms: [
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+        {},
+        {},
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+        {},
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+        { impHs: 1 },
+        {},
+      ],
+      bonds: [
+        { bo: 2, atoms: [0, 1] },
+        { atoms: [1, 2] },
+        { bo: 2, atoms: [2, 3] },
+        { atoms: [3, 4] },
+        { bo: 2, atoms: [4, 5] },
+        { atoms: [4, 6] },
+        { bo: 2, atoms: [6, 7] },
+        { atoms: [7, 8] },
+        { bo: 2, atoms: [8, 9] },
+        { atoms: [9, 10] },
+        { bo: 2, atoms: [10, 11] },
+        { atoms: [11, 12] },
+        { bo: 2, atoms: [12, 13] },
+        { atoms: [5, 0] },
+        { atoms: [13, 8] },
+        { atoms: [13, 3] },
+      ],
+      extensions: [{
+        name: 'rdkitRepresentation',
+        formatVersion: 2,
+        toolkitVersion: '2025.03.4',
+        aromaticAtoms: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+        aromaticBonds: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+        cipRanks: [0, 1, 4, 6, 5, 2, 3, 3, 5, 2, 0, 1, 4, 6],
+        atomRings: [[0, 5, 4, 3, 2, 1], [6, 7, 8, 13, 3, 4], [9, 10, 11, 12, 13, 8]],
+      }],
+    }],
+  });
 });
