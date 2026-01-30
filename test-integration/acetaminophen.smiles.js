bb4e40ea787fec2a32d42cd96a5ac69dae00fcc5
Souradeep Nanda <souradeep.nanda123@gmail.com>
2026-01-30 19:13:40 +0530

    Update to new API

--- test-integration/acetaminophen.smiles.js
+++ test-integration/acetaminophen.smiles.js
@@ -1,27 +1,39 @@
 // Acetaminophen and Related Compounds
 // Non-NSAID analgesics and antipyretics
 
-import { Fragment, Ring } from '../src/fragment';
+import {
+  Fragment, Ring, Linear, Molecule,
+} from '../src/index.js';
 
 // === FRAGMENTS ===
 
 // Acetamido: CH3-C(=O)-NH- -> CC(=O)N
-export const acetamidoLeft = Fragment('CC(=O)N');
-export const acetamidoRight = Fragment('NC(=O)C');
-export const acetamido = acetamidoLeft;
+export const acetamido = Fragment('CC(=O)N');
 
-export const benzene = Ring('c', 6);
+// Benzene ring
+export const benzene = Ring({ atoms: 'c', size: 6 });
+
+// Hydroxyl group
+export const hydroxyl = Linear(['O']);
+
+// Para-hydroxybenzene (phenol at position 4)
+export const paraHydroxybenzene = benzene.attach(hydroxyl, 4);
 
 // === ACETAMINOPHEN ===
 // SMILES: CC(=O)NC1=CC=C(O)C=C1
 
 export const acetaminophen = Fragment('CC(=O)Nc1ccc(O)cc1');
-export const acetaminophenNonCannon = benzene.attachAt(1, acetamidoRight).attachAt(4, 'O');
+export const acetaminophenBuilt = Molecule([acetamido, paraHydroxybenzene]);
 export const paracetamol = Fragment('CC(=O)Nc1ccc(O)cc1');
 
 // === PHENACETIN ===
 // SMILES: CC(=O)NC1=CC=C(OCC)C=C1
 
-export const ethoxy = Fragment('OCC');
+// Ethoxy group: -O-CH2-CH3
+export const ethoxy = Linear(['O', 'C', 'C']);
+
+// Para-ethoxybenzene (ethoxy at position 4)
+export const paraEthoxybenzene = Ring({ atoms: 'c', size: 6 }).attach(ethoxy, 4);
+
 export const phenacetin = Fragment('CC(=O)Nc1ccc(OCC)cc1');
-export const phenacetinNonCannon = benzene.attachAt(1, acetamidoRight).attachAt(4, ethoxy);
+export const phenacetinBuilt = Molecule([acetamido, paraEthoxybenzene]);
