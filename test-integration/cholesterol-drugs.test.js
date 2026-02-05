eb7ef1b107cdc17c6b21dff057a0bf879013c8d3
Souradeep Nanda <souradeep.nanda123@gmail.com>
2026-02-05 12:49:50 +0530

    Add common cholesterol drugs

---
+++ test-integration/cholesterol-drugs.test.js
@@ -0,0 +1,414 @@
+import { describe, test, expect } from 'bun:test';
+import { parse } from '../src/parser/index.js';
+import { stripExports, createFunction, executeCode } from './utils.js';
+
+const ATORVASTATIN_SMILES = 'CC(C)c1c(C(=O)Nc2ccccc2)c(-c2ccccc2)c(-c2ccc(F)cc2)n1CC[C@@H](O)C[C@@H](O)CC(=O)O';
+const SIMVASTATIN_SMILES = 'CCC(C)C(=O)OC1CC(C)C=C2C=CC(C)C(CCC3CC(O)CC(=O)O3)C12';
+const ROSUVASTATIN_SMILES = 'CC(C)c1nc(nc(c1/C=C/[C@@H](O)C[C@@H](O)CC(=O)O)c2ccc(F)cc2)N(C)S(=O)(=O)C';
+const PRAVASTATIN_SMILES = 'CCC(C)C(=O)O[C@H]1C[C@H](O)C=C2C=C[C@H](C)[C@H](CC[C@@H](O)CC(=O)O)[C@H]12';
+const LOVASTATIN_SMILES = 'CCC(C)C(=O)OC1CC(C)C=C2C=CC(C)C(CCC3CC(O)CC(=O)O3)C12';
+const FLUVASTATIN_SMILES = 'CC(C)n1c(C(=O)Nc2ccccc2C(=O)O)c(-c2ccc(F)cc2)c(O)c1/C=C/c1ccccc1';
+const EZETIMIBE_SMILES = 'C[C@H]1[C@@H](O)[C@H](O[C@H]1c2ccc(F)cc2)c3ccc(O)cc3C(=O)N4CCC(CC4)c5ccc(F)cc5';
+const FENOFIBRATE_SMILES = 'CC(C)OC(=O)C(C)(C)Oc1ccc(cc1)C(=O)c2ccc(Cl)cc2';
+const GEMFIBROZIL_SMILES = 'CC1=CC(C)=CC=C1CCCC(C)(C)C(=O)O';
+const PITAVASTATIN_SMILES = 'CC(C)c1nc(nc(c1/C=C/[C@@H](O)C[C@@H](O)CC(=O)O)c2ccc(F)cc2)NC(=O)C3CC3';
+
+describe('Atorvastatin Integration Test', () => {
+  test('parses atorvastatin', () => {
+    const ast = parse(ATORVASTATIN_SMILES);
+    const obj = ast.toObject();
+    expect(obj).toMatchSnapshot();
+    expect(ast.smiles).toBe(ATORVASTATIN_SMILES);
+  });
+
+  test('generates valid code via toCode()', () => {
+    const ast = parse(ATORVASTATIN_SMILES);
+    const code = ast.toCode('v');
+    expect(code).toMatchSnapshot();
+  });
+
+  test('generated code is valid JavaScript', () => {
+    const ast = parse(ATORVASTATIN_SMILES);
+    const code = ast.toCode('v');
+    const executableCode = stripExports(code);
+
+    let factory;
+    expect(() => {
+      factory = createFunction('Ring', 'Linear', 'FusedRing', 'Molecule', executableCode);
+    }).not.toThrow();
+    expect(typeof factory).toBe('function');
+  });
+
+  test('codegen round-trip: generated code produces valid SMILES', () => {
+    const ast = parse(ATORVASTATIN_SMILES);
+    const code = ast.toCode('v');
+    const executableCode = stripExports(code);
+
+    const varMatch = code.match(/export const (v\d+) = /g);
+    const lastVar = varMatch ? varMatch[varMatch.length - 1].match(/export const (v\d+)/)[1] : 'v1';
+
+    const reconstructed = executeCode(executableCode, lastVar);
+
+    expect(reconstructed.smiles).toBe(ATORVASTATIN_SMILES);
+  });
+});
+
+describe('Simvastatin Integration Test', () => {
+  test('parses simvastatin', () => {
+    const ast = parse(SIMVASTATIN_SMILES);
+    const obj = ast.toObject();
+    expect(obj).toMatchSnapshot();
+    expect(ast.smiles).toBe(SIMVASTATIN_SMILES);
+  });
+
+  test('generates valid code via toCode()', () => {
+    const ast = parse(SIMVASTATIN_SMILES);
+    const code = ast.toCode('v');
+    expect(code).toMatchSnapshot();
+  });
+
+  test('generated code is valid JavaScript', () => {
+    const ast = parse(SIMVASTATIN_SMILES);
+    const code = ast.toCode('v');
+    const executableCode = stripExports(code);
+
+    let factory;
+    expect(() => {
+      factory = createFunction('Ring', 'Linear', 'FusedRing', 'Molecule', executableCode);
+    }).not.toThrow();
+    expect(typeof factory).toBe('function');
+  });
+
+  test('codegen round-trip: generated code produces valid SMILES', () => {
+    const ast = parse(SIMVASTATIN_SMILES);
+    const code = ast.toCode('v');
+    const executableCode = stripExports(code);
+
+    const varMatch = code.match(/export const (v\d+) = /g);
+    const lastVar = varMatch ? varMatch[varMatch.length - 1].match(/export const (v\d+)/)[1] : 'v1';
+
+    const reconstructed = executeCode(executableCode, lastVar);
+
+    expect(reconstructed.smiles).toBe(SIMVASTATIN_SMILES);
+  });
+});
+
+describe('Rosuvastatin Integration Test', () => {
+  test('parses rosuvastatin', () => {
+    const ast = parse(ROSUVASTATIN_SMILES);
+    const obj = ast.toObject();
+    expect(obj).toMatchSnapshot();
+    expect(ast.smiles).toBe(ROSUVASTATIN_SMILES);
+  });
+
+  test('generates valid code via toCode()', () => {
+    const ast = parse(ROSUVASTATIN_SMILES);
+    const code = ast.toCode('v');
+    expect(code).toMatchSnapshot();
+  });
+
+  test('generated code is valid JavaScript', () => {
+    const ast = parse(ROSUVASTATIN_SMILES);
+    const code = ast.toCode('v');
+    const executableCode = stripExports(code);
+
+    let factory;
+    expect(() => {
+      factory = createFunction('Ring', 'Linear', 'FusedRing', 'Molecule', executableCode);
+    }).not.toThrow();
+    expect(typeof factory).toBe('function');
+  });
+
+  test('codegen round-trip: generated code produces valid SMILES', () => {
+    const ast = parse(ROSUVASTATIN_SMILES);
+    const code = ast.toCode('v');
+    const executableCode = stripExports(code);
+
+    const varMatch = code.match(/export const (v\d+) = /g);
+    const lastVar = varMatch ? varMatch[varMatch.length - 1].match(/export const (v\d+)/)[1] : 'v1';
+
+    const reconstructed = executeCode(executableCode, lastVar);
+
+    expect(reconstructed.smiles).toBe(ROSUVASTATIN_SMILES);
+  });
+});
+
+describe('Pravastatin Integration Test', () => {
+  test('parses pravastatin', () => {
+    const ast = parse(PRAVASTATIN_SMILES);
+    const obj = ast.toObject();
+    expect(obj).toMatchSnapshot();
+    expect(ast.smiles).toBe(PRAVASTATIN_SMILES);
+  });
+
+  test('generates valid code via toCode()', () => {
+    const ast = parse(PRAVASTATIN_SMILES);
+    const code = ast.toCode('v');
+    expect(code).toMatchSnapshot();
+  });
+
+  test('generated code is valid JavaScript', () => {
+    const ast = parse(PRAVASTATIN_SMILES);
+    const code = ast.toCode('v');
+    const executableCode = stripExports(code);
+
+    let factory;
+    expect(() => {
+      factory = createFunction('Ring', 'Linear', 'FusedRing', 'Molecule', executableCode);
+    }).not.toThrow();
+    expect(typeof factory).toBe('function');
+  });
+
+  test('codegen round-trip: generated code produces valid SMILES', () => {
+    const ast = parse(PRAVASTATIN_SMILES);
+    const code = ast.toCode('v');
+    const executableCode = stripExports(code);
+
+    const varMatch = code.match(/export const (v\d+) = /g);
+    const lastVar = varMatch ? varMatch[varMatch.length - 1].match(/export const (v\d+)/)[1] : 'v1';
+
+    const reconstructed = executeCode(executableCode, lastVar);
+
+    expect(reconstructed.smiles).toBe(PRAVASTATIN_SMILES);
+  });
+});
+
+describe('Lovastatin Integration Test', () => {
+  test('parses lovastatin', () => {
+    const ast = parse(LOVASTATIN_SMILES);
+    const obj = ast.toObject();
+    expect(obj).toMatchSnapshot();
+    expect(ast.smiles).toBe(LOVASTATIN_SMILES);
+  });
+
+  test('generates valid code via toCode()', () => {
+    const ast = parse(LOVASTATIN_SMILES);
+    const code = ast.toCode('v');
+    expect(code).toMatchSnapshot();
+  });
+
+  test('generated code is valid JavaScript', () => {
+    const ast = parse(LOVASTATIN_SMILES);
+    const code = ast.toCode('v');
+    const executableCode = stripExports(code);
+
+    let factory;
+    expect(() => {
+      factory = createFunction('Ring', 'Linear', 'FusedRing', 'Molecule', executableCode);
+    }).not.toThrow();
+    expect(typeof factory).toBe('function');
+  });
+
+  test('codegen round-trip: generated code produces valid SMILES', () => {
+    const ast = parse(LOVASTATIN_SMILES);
+    const code = ast.toCode('v');
+    const executableCode = stripExports(code);
+
+    const varMatch = code.match(/export const (v\d+) = /g);
+    const lastVar = varMatch ? varMatch[varMatch.length - 1].match(/export const (v\d+)/)[1] : 'v1';
+
+    const reconstructed = executeCode(executableCode, lastVar);
+
+    expect(reconstructed.smiles).toBe(LOVASTATIN_SMILES);
+  });
+});
+
+describe('Fluvastatin Integration Test', () => {
+  test('parses fluvastatin', () => {
+    const ast = parse(FLUVASTATIN_SMILES);
+    const obj = ast.toObject();
+    expect(obj).toMatchSnapshot();
+    expect(ast.smiles).toBe(FLUVASTATIN_SMILES);
+  });
+
+  test('generates valid code via toCode()', () => {
+    const ast = parse(FLUVASTATIN_SMILES);
+    const code = ast.toCode('v');
+    expect(code).toMatchSnapshot();
+  });
+
+  test('generated code is valid JavaScript', () => {
+    const ast = parse(FLUVASTATIN_SMILES);
+    const code = ast.toCode('v');
+    const executableCode = stripExports(code);
+
+    let factory;
+    expect(() => {
+      factory = createFunction('Ring', 'Linear', 'FusedRing', 'Molecule', executableCode);
+    }).not.toThrow();
+    expect(typeof factory).toBe('function');
+  });
+
+  test('codegen round-trip: generated code produces valid SMILES', () => {
+    const ast = parse(FLUVASTATIN_SMILES);
+    const code = ast.toCode('v');
+    const executableCode = stripExports(code);
+
+    const varMatch = code.match(/export const (v\d+) = /g);
+    const lastVar = varMatch ? varMatch[varMatch.length - 1].match(/export const (v\d+)/)[1] : 'v1';
+
+    const reconstructed = executeCode(executableCode, lastVar);
+
+    expect(reconstructed.smiles).toBe(FLUVASTATIN_SMILES);
+  });
+});
+
+describe('Ezetimibe Integration Test', () => {
+  test('parses ezetimibe', () => {
+    const ast = parse(EZETIMIBE_SMILES);
+    const obj = ast.toObject();
+    expect(obj).toMatchSnapshot();
+    expect(ast.smiles).toBe(EZETIMIBE_SMILES);
+  });
+
+  test('generates valid code via toCode()', () => {
+    const ast = parse(EZETIMIBE_SMILES);
+    const code = ast.toCode('v');
+    expect(code).toMatchSnapshot();
+  });
+
+  test('generated code is valid JavaScript', () => {
+    const ast = parse(EZETIMIBE_SMILES);
+    const code = ast.toCode('v');
+    const executableCode = stripExports(code);
+
+    let factory;
+    expect(() => {
+      factory = createFunction('Ring', 'Linear', 'FusedRing', 'Molecule', executableCode);
+    }).not.toThrow();
+    expect(typeof factory).toBe('function');
+  });
+
+  test('codegen round-trip: generated code produces valid SMILES', () => {
+    const ast = parse(EZETIMIBE_SMILES);
+    const code = ast.toCode('v');
+    const executableCode = stripExports(code);
+
+    const varMatch = code.match(/export const (v\d+) = /g);
+    const lastVar = varMatch ? varMatch[varMatch.length - 1].match(/export const (v\d+)/)[1] : 'v1';
+
+    const reconstructed = executeCode(executableCode, lastVar);
+
+    expect(reconstructed.smiles).toBe(EZETIMIBE_SMILES);
+  });
+});
+
+describe('Fenofibrate Integration Test', () => {
+  test('parses fenofibrate', () => {
+    const ast = parse(FENOFIBRATE_SMILES);
+    const obj = ast.toObject();
+    expect(obj).toMatchSnapshot();
+    expect(ast.smiles).toBe(FENOFIBRATE_SMILES);
+  });
+
+  test('generates valid code via toCode()', () => {
+    const ast = parse(FENOFIBRATE_SMILES);
+    const code = ast.toCode('v');
+    expect(code).toMatchSnapshot();
+  });
+
+  test('generated code is valid JavaScript', () => {
+    const ast = parse(FENOFIBRATE_SMILES);
+    const code = ast.toCode('v');
+    const executableCode = stripExports(code);
+
+    let factory;
+    expect(() => {
+      factory = createFunction('Ring', 'Linear', 'FusedRing', 'Molecule', executableCode);
+    }).not.toThrow();
+    expect(typeof factory).toBe('function');
+  });
+
+  test('codegen round-trip: generated code produces valid SMILES', () => {
+    const ast = parse(FENOFIBRATE_SMILES);
+    const code = ast.toCode('v');
+    const executableCode = stripExports(code);
+
+    const varMatch = code.match(/export const (v\d+) = /g);
+    const lastVar = varMatch ? varMatch[varMatch.length - 1].match(/export const (v\d+)/)[1] : 'v1';
+
+    const reconstructed = executeCode(executableCode, lastVar);
+
+    expect(reconstructed.smiles).toBe(FENOFIBRATE_SMILES);
+  });
+});
+
+describe('Gemfibrozil Integration Test', () => {
+  test('parses gemfibrozil', () => {
+    const ast = parse(GEMFIBROZIL_SMILES);
+    const obj = ast.toObject();
+    expect(obj).toMatchSnapshot();
+    expect(ast.smiles).toBe(GEMFIBROZIL_SMILES);
+  });
+
+  test('generates valid code via toCode()', () => {
+    const ast = parse(GEMFIBROZIL_SMILES);
+    const code = ast.toCode('v');
+    expect(code).toMatchSnapshot();
+  });
+
+  test('generated code is valid JavaScript', () => {
+    const ast = parse(GEMFIBROZIL_SMILES);
+    const code = ast.toCode('v');
+    const executableCode = stripExports(code);
+
+    let factory;
+    expect(() => {
+      factory = createFunction('Ring', 'Linear', 'FusedRing', 'Molecule', executableCode);
+    }).not.toThrow();
+    expect(typeof factory).toBe('function');
+  });
+
+  test('codegen round-trip: generated code produces valid SMILES', () => {
+    const ast = parse(GEMFIBROZIL_SMILES);
+    const code = ast.toCode('v');
+    const executableCode = stripExports(code);
+
+    const varMatch = code.match(/export const (v\d+) = /g);
+    const lastVar = varMatch ? varMatch[varMatch.length - 1].match(/export const (v\d+)/)[1] : 'v1';
+
+    const reconstructed = executeCode(executableCode, lastVar);
+
+    expect(reconstructed.smiles).toBe(GEMFIBROZIL_SMILES);
+  });
+});
+
+describe('Pitavastatin Integration Test', () => {
+  test('parses pitavastatin', () => {
+    const ast = parse(PITAVASTATIN_SMILES);
+    const obj = ast.toObject();
+    expect(obj).toMatchSnapshot();
+    expect(ast.smiles).toBe(PITAVASTATIN_SMILES);
+  });
+
+  test('generates valid code via toCode()', () => {
+    const ast = parse(PITAVASTATIN_SMILES);
+    const code = ast.toCode('v');
+    expect(code).toMatchSnapshot();
+  });
+
+  test('generated code is valid JavaScript', () => {
+    const ast = parse(PITAVASTATIN_SMILES);
+    const code = ast.toCode('v');
+    const executableCode = stripExports(code);
+
+    let factory;
+    expect(() => {
+      factory = createFunction('Ring', 'Linear', 'FusedRing', 'Molecule', executableCode);
+    }).not.toThrow();
+    expect(typeof factory).toBe('function');
+  });
+
+  test('codegen round-trip: generated code produces valid SMILES', () => {
+    const ast = parse(PITAVASTATIN_SMILES);
+    const code = ast.toCode('v');
+    const executableCode = stripExports(code);
+
+    const varMatch = code.match(/export const (v\d+) = /g);
+    const lastVar = varMatch ? varMatch[varMatch.length - 1].match(/export const (v\d+)/)[1] : 'v1';
+
+    const reconstructed = executeCode(executableCode, lastVar);
+
+    expect(reconstructed.smiles).toBe(PITAVASTATIN_SMILES);
+  });
+});
